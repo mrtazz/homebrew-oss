74c6686f5c975879906ecb6e6d27a256f8014856
Daniel Schauenberg <d@unwiredcouch.com>
2016-05-12 12:37:57 -0400

    fix sha256 for leoric formula

--- leoric.rb
+++ leoric.rb
@@ -2,7 +2,7 @@ class Leoric < Formula
   desc "Project initialization from skeleton files"
   homepage "http://code.mrtazz.com/leoric/"
   url "https://github.com/mrtazz/leoric/archive/0.2.3.tar.gz"
-  sha256 "1cdca050f4d9e0c4baff339562bb8c38641d9211"
+  sha256 "45ac0e326d29dcb9eecf0933e4eabc46e5b21c59bc75db4e351ffa5bf4f7d02c"
 
   depends_on "pandoc"
 
