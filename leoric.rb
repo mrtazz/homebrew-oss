ac0c1b3cf6abc95d479dbfb8f8b76582f09e6dc0
Daniel Schauenberg <d@unwiredcouch.com>
2016-05-12 09:34:41 -0400

    update leoric formula to new release

--- leoric.rb
+++ leoric.rb
@@ -1,8 +1,8 @@
 class Leoric < Formula
   desc "Project initialization from skeleton files"
   homepage "http://code.mrtazz.com/leoric/"
-  url "https://github.com/mrtazz/leoric/archive/0.2.1.tar.gz"
-  sha256 "43f880280af70a778ebd1dce978df643780bec7a3bc4014398ebd6104c794f40"
+  url "https://github.com/mrtazz/leoric/archive/0.2.2.tar.gz"
+  sha256 "09a4c1c954b1a410cbbb7713b77a94af2f9762881482748a51015f2d97898f55"
 
   depends_on "txt2man"
 
