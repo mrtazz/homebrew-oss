a2c68ae847b46a3b14ae3010669fc2c127e3b1e1
Daniel Schauenberg <d@unwiredcouch.com>
2024-01-26 22:15:53 +0100

    update check text

--- script/check_for_update.sh
+++ script/check_for_update.sh
@@ -5,7 +5,7 @@ project="${1}"
 newest_version=$(curl -sLf https://api.github.com/repos/mrtazz/${project}/git/refs/tags | jq -r '. | last | .ref' | awk -F '/' '{ print $3 }')
 
 if grep -q "${newest_version}" "${project}.rb"; then
-  echo "Everything up to date for ${project}."
+  echo "${project} is up to date."
 else
   echo "${project} needs to be updated to version ${newest_version}."
 fi
