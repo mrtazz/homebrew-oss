89d712d749fcfce304c58424cfd21d1f9bf9e14c
Daniel Schauenberg <d@unwiredcouch.com>
2024-01-30 22:02:25 +0100

    update mae to v0.1.1

--- mae.rb
+++ mae.rb
@@ -1,8 +1,8 @@
 class Mae < Formula
   desc "Mail attachment extractor command-line tool"
   homepage "https://github.com/mrtazz/mae"
-  url "https://github.com/mrtazz/mae/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "6098528c1abc15afcde03965c32d0394975fcefe2cb4e39e95375f3acbba2fe7"
+  url "https://github.com/mrtazz/mae/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "32b18b6765b9c3c11fa9f3fdf3c98c8244d83db7497a9fcbca1c0678b975fc97"
   license "MIT"
 
   depends_on "pandoc" => :build
